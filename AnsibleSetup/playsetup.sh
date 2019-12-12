24885f85208f8d2f5a7c7ab54182f2f00edf2083
Aditya Kumar <aditya.kumar@salesforce.com>
2019-12-12 09:40:08 -0500

    added teamcity agentcode+missing templatefile

--- AnsibleSetup/playsetup.sh
+++ AnsibleSetup/playsetup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-ansible-playbook -i inventory pb_Install_splunk_forwarder.yml &
-ansible-playbook -i inventory pb_Install_splunk_Indexer.yml &
+ansible-playbook -i inventory pb_Install_splunk_forwarder.yml
